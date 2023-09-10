50d58c2a243c1ca22709862a2fc8db92f9c6d83f
Anne Wynter <missannemary1@gmail.com>
2023-09-10 18:00:58 +0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,138 @@
-# ReactPortfolio
+# React Portfolio
+
+## Description
+
+The GitHub-Pages launched application showcases Anne Mary Wynter's React Portfolio, which contains (6) highlighted projects from her Full-Stack Developer boot camp experience at Rutgers University.
+
+## Table of Contents
+
+- [Overview](#overview)
+- [The Challenge](#the-challenge)
+- [Usage Information](#usage-information)
+- [Installation Process](#installation-process)
+- [Built With](#built-with)
+- [What I Learned](#what-i-learned)
+- [Continued Development](#continued-development)
+- [Author](#author)
+
+## Overview
+
+### The Challenge
+
+The main objective of this project is to create a single-page application portfolio for a web developer, showcasing Anne Mary Wynter's experience and proficiency in building React applications.
+
+1. Create a dynamic and user-friendly interface.
+2. Implement smooth navigation between different sections of the portfolio.
+3. Include a contact form with validation and error notifications.
+4. Integrate links to Anne Mary Wynter's resume and social media profiles.
+
+### Assigned User Story:
+
+```
+AS AN employer looking for candidates with experience building single-page applications
+I WANT to view a potential employee's deployed React portfolio of work samples
+SO THAT I can assess whether they're a good candidate for an open position
+```
+
+
+### Acceptance Criteria:
+
+- GIVEN a single-page application portfolio for a web developer
+- WHEN I load the portfolio
+- THEN I am presented with a page containing a header, a section for content, and a footer
+- WHEN I view the header
+- THEN I am presented with the developer's name and navigation with titles corresponding to different sections of the portfolio
+- WHEN I view the navigation titles
+- THEN I am presented with the titles About Me, Portfolio, Contact, and Resume, and the title corresponding to the current section is highlighted
+- WHEN I click on a navigation title
+- THEN I am presented with the corresponding section below the navigation without the page reloading and that title is highlighted
+- WHEN I load the portfolio the first time
+- THEN the About Me title and section are selected by default
+- WHEN I am presented with the About Me section
+- THEN I see a recent photo or avatar of the developer and a short bio about them
+- WHEN I am presented with the Portfolio section
+- THEN I see titled images of six of the developer’s applications with links to both the deployed applications and the corresponding GitHub repositories
+- WHEN I am presented with the Contact section
+- THEN I see a contact form with fields for a name, an email address, and a message
+- WHEN I move my cursor out of one of the form fields without entering text
+- THEN I receive a notification that this field is required
+- WHEN I enter text into the email address field
+- THEN I receive a notification if I have entered an invalid email address
+- WHEN I am presented with the Resume section
+- THEN I see a link to a downloadable resume and a list of the developer’s proficiencies
+- WHEN I view the footer
+- THEN I am presented with text or icon links to the developer’s GitHub and LinkedIn profiles, and their profile on a third platform (Stack Overflow, Twitter)
+
+## React Portfolio
+
+### Usage Instructions
+
+1. Navigate to the deployed application link.
+2. Click on the navigation titles to switch between sections.
+3. View Anne Mary Wynter's work samples in the Portfolio section.
+4. Fill out the contact form in the Contact section.
+5. Download Anne Mary Wynter's resume in the Resume section.
+
+### Deployed GitHub-Pages Application Link
+
+[Deployed React Portfolio](https://annemarywynter1986.github.io/ReactPortfolio/)
+
+### GitHub Repository
+
+[GitHub Repository](https://github.com/annemarywynter1986/ReactPortfolio.git) 
+
+### React Portfolio Screenshots
+
+<!-- Include screenshots or GIFs of portfolio here -->
+
+### Installation Process
+
+1. Clone the Repository from GitHub (or) Download Zip Folder from Repository from GitHub.
+2. Open the cloned (or downloaded) repository in any source code editor.
+3. Open the integrated terminal of the document and complete the respective installation guides provided in "Built With" to ensure the cloned documentation will operate.
+
+### Built With
+
+- Dynamic JavaScript
+- How To Create React.md: [Click to Preview Document](./src/documentation/HowToCreateReact.md)
+- JSON: [JSON](https://www.npmjs.com/package/json)
+- Mongoose: [7.0.3](https://www.npmjs.com/package/mongoose)
+- Express: [4.18.2](https://www.npmjs.com/package/express)
+- Node.js: [16.18.1](https://nodejs.org/en/blog/release/v16.18.1/)
+- MongoDB: [Website](https://www.mongodb.com/)
+- Nodemon: [2.0.12](https://www.npmjs.com/package/nodemon/v/2.0.12)
+- Fontawesome: [6.4.0](https://www.npmjs.com/package/@fortawesome/)
+- dotenv: [16.0.3](https://www.npmjs.com/package/dotenv)
+- Express: [4.18.2](https://www.npmjs.com/package/express)
+- GraphQL: [16.6.0](https://www.npmjs.com/package/graphql)
+- JsonWebToken: [9.0.0](https://www.npmjs.com/package/jsonwebtoken)
+- Mongodb: [5.1.0](https://www.npmjs.com/package/mongodb)
+- Mongoose: [7.0.3](https://www.npmjs.com/package/mongoose)
+- React: [18.2.0](https://www.npmjs.com/package/react)
+- React-Dom: [18.2.0](https://www.npmjs.com/package/react-dom)
+- React: [18.2.0](https://reactjs.org)
+- GraphQL: [16.6.0](https://graphql.org)
+- MongoDB: [5.1.0](https://www.mongodb.com)
+- Mongoose ODM: [7.0.3](https://mongoosejs.com)
+- JWT: [9.0.0](https://jwt.io)
+- Heroku: [Website](https://www.heroku.com/platform)
+- License Badge: [Shields.io](https://shields.io/)
+- Visual Studio Code: [Website](https://code.visualstudio.com/)
+
+### What I Learned
+
+1. Building a single-page application using React.
+2. Implementing smooth navigation using React Router.
+3. Creating a responsive and visually appealing interface.
+4. Integrating form validation and error handling in React applications.
+
+### Continued Development
+
+1. Add a blog section to showcase my thoughts and experiences.
+2. Implement a search functionality for easier navigation through work samples.
+3. Incorporate testimonials from previous clients or employers.
+4. Integrate a dark mode toggle for better accessibility and user experience.
+
+## Author
+
+Anne Mary Wynter
--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -1,112 +1,123 @@
-import React from "react";
-import { Container, Grid, Paper, Typography, Button } from "@mui/material";
-import PDF from "../resume/documentation/Anne Mary Software Developer Resume for webapp.pdf"; 
+import React, { useState, useEffect } from "react";
+import PDF from "../resume/documentation/Anne Mary Software Developer Resume for webapp.pdf";
+import { Container, Grid, Paper, Typography, Button, Box } from "@mui/material";
+import "./Resume.module.scss";
 
 function Resume() {
-  return (
-    <section id="resume" className="resume-margin">
-      <Container maxWidth="md">
-        <Paper elevation={3} style={{ padding: "20px", marginBottom: "20px" }}>
-          <Typography variant="h4" align="center">
-            Resume
-          </Typography>
-          <hr style={{ backgroundColor: "#333" }} />
-          <Button variant="contained" color="primary" href={PDF} download>
-            Download Resume
-          </Button>
-        </Paper>
+    const [darkMode] = useState(
+        JSON.parse(localStorage.getItem("darkMode")) || false
+    );
 
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Paper elevation={3} style={{ padding: "20px" }}>
-              <Typography variant="h6">Technical Skills</Typography>
-              <hr style={{ backgroundColor: "#333" }} />
-              <ul>
-                <li>
-                  <strong>Languages:</strong> JavaScript, HTML, CSS
-                </li>
-                <li>
-                  <strong>Libraries/Frameworks:</strong> React, jQuery,
-                  Material UI
-                </li>
-                <li>
-                  <strong>Databases:</strong> MongoDB, MySQL
-                </li>
-                <li>
-                  <strong>Tools/Platforms:</strong> Git, GitHub, Heroku
-                </li>
-              </ul>
-            </Paper>
-          </Grid>
+    useEffect(() => {
+        localStorage.setItem("darkMode", JSON.stringify(darkMode));
+    }, [darkMode]);
 
-          <Grid item xs={12}>
-            <Paper elevation={3} style={{ padding: "20px" }}>
-              <Typography variant="h6">Education</Typography>
-              <hr style={{ backgroundColor: "#333" }} />
-              <div className="resume-item">
-                <Typography variant="subtitle1">
-                  Certificate. Full Stack Developer
-                </Typography>
-                <Typography variant="subtitle2">
-                  Rutgers University, APRIL 2023- OCTOBER 2023
-                </Typography>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer cursus bibendum magna, vitae dapibus felis ultricies
-                  ut. Sed ac odio eget est convallis ullamcorper. Duis bibendum
-                  vestibulum risus, non convallis odio aliquam eu.
-                </Typography>
-              </div>
-            </Paper>
-          </Grid>
+    return (
+        <Box className={`resume-container ${darkMode ? "dark" : "light"}`}>
+            <section id="resume" className="resume-margin">
+                <Container maxWidth="md">
+                    <Paper elevation={3} className="resume-paper">
+                        <Typography variant="h4" align="center">
+                            Resume
+                        </Typography>
+                        <hr />
+                        <Button variant="contained" color="primary" href={PDF} download>
+                            Download Resume
+                        </Button>
+                    </Paper>
 
-          <Grid item xs={12}>
-            <Paper elevation={3} style={{ padding: "20px" }}>
-              <Typography variant="h6">Work Experience</Typography>
-              <hr style={{ backgroundColor: "#333" }} />
-              <div className="resume-item">
-                <Typography variant="subtitle1">
-                  QA automation engineer, Nextiva Inc
-                </Typography>
-                <Typography variant="subtitle2">
-                  Arizona (remote), 2022-present
-                </Typography>
-                <Typography>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Integer cursus bibendum magna, vitae dapibus felis ultricies
-                  ut. Sed ac odio eget est convallis ullamcorper. Duis bibendum
-                  vestibulum risus, non convallis odio aliquam eu.
-                </Typography>
-              </div>
-            </Paper>
-          </Grid>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Paper elevation={3} className={`resume-paper ${darkMode ? 'dark' : 'light'}`}>
+                                <Typography variant="h6">Technical Skills</Typography>
+                                <hr className={darkMode ? 'dark' : 'light'} />
+                                <ul>
+                                    <li>
+                                        <strong>Languages:</strong> JavaScript, HTML, CSS
+                                    </li>
+                                    <li>
+                                        <strong>Libraries/Frameworks:</strong> React, jQuery, Material UI
+                                    </li>
+                                    <li>
+                                        <strong>Databases:</strong> MongoDB, MySQL
+                                    </li>
+                                    <li>
+                                        <strong>Tools/Platforms:</strong> Git, GitHub, Heroku
+                                    </li>
+                                </ul>
+                            </Paper>
 
-          <Grid item xs={12}>
-            <Paper elevation={3} style={{ padding: "20px" }}>
-              <Typography variant="h6">Proficiencies</Typography>
-              <ul>
-                <li>HTML/CSS</li>
-                <li>JavaScript</li>
-                <li>
-                  <span style={{ color: "#009688" }}>M</span>ongoDB
-                </li>
-                <li>
-                  <span style={{ color: "#009688" }}>E</span>xpress
-                </li>
-                <li>
-                  <span style={{ color: "#009688" }}>R</span>eact
-                </li>
-                <li>
-                  <span style={{ color: "#009688" }}>N</span>ode.js
-                </li>
-                <li>SQL</li>
-              </ul>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
-    </section>
-  );
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Paper elevation={3} className="resume-paper">
+                                <Typography variant="h6">Education</Typography>
+                                <hr />
+                                <div className="resume-item">
+                                    <Typography variant="subtitle1">
+                                        Certificate. Full Stack Developer
+                                    </Typography>
+                                    <Typography variant="subtitle2">
+                                        Rutgers University, APRIL 2023- OCTOBER 2023
+                                    </Typography>
+                                    <Typography>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                        Integer cursus bibendum magna, vitae dapibus felis ultricies
+                                        ut. Sed ac odio eget est convallis ullamcorper. Duis bibendum
+                                        vestibulum risus, non convallis odio aliquam eu.
+                                    </Typography>
+                                </div>
+                            </Paper>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Paper elevation={3} className="resume-paper">
+                                <Typography variant="h6">Work Experience</Typography>
+                                <hr />
+                                <div className="resume-item">
+                                    <Typography variant="subtitle1">
+                                        QA automation engineer, Nextiva Inc
+                                    </Typography>
+                                    <Typography variant="subtitle2">
+                                        Arizona (remote), 2022-present
+                                    </Typography>
+                                    <Typography>
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                                        Integer cursus bibendum magna, vitae dapibus felis ultricies
+                                        ut. Sed ac odio eget est convallis ullamcorper. Duis bibendum
+                                        vestibulum risus, non convallis odio aliquam eu.
+                                    </Typography>
+                                </div>
+                            </Paper>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Paper elevation={3} className="resume-paper">
+                                <Typography variant="h6">Proficiencies</Typography>
+                                <ul>
+                                    <li>HTML/CSS</li>
+                                    <li>JavaScript</li>
+                                    <li>
+                                        <span style={{ color: "#009688" }}>M</span>ongoDB
+                                    </li>
+                                    <li>
+                                        <span style={{ color: "#009688" }}>E</span>xpress
+                                    </li>
+                                    <li>
+                                        <span style={{ color: "#009688" }}>R</span>eact
+                                    </li>
+                                    <li>
+                                        <span style={{ color: "#009688" }}>N</span>ode.js
+                                    </li>
+                                    <li>SQL</li>
+                                </ul>
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </section>
+        </Box>
+    );
 }
 
 export default Resume;
